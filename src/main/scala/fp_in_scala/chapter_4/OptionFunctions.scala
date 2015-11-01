2cdcb03e74f9d2b18070bff43d73653683741afe
Alex Garland <alex.d.garland@gmail.com>
2015-11-01 16:20:02 +0000

    Ex. 4.4 - Implement sequence

--- src/main/scala/fp_in_scala/chapter_4/OptionFunctions.scala
+++ src/main/scala/fp_in_scala/chapter_4/OptionFunctions.scala
@@ -2,12 +2,36 @@ package fp_in_scala.chapter_4
 
 object OptionFunctions {
 
-  // Exercise 4.4 - implement "map2",
-  // applying  a binary function if both Options given are Some
+  // Exercise 4.3 - implement "map2",
+  // applying a binary function if both Options given are Some
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B)=>C) : Option[C] =
     (a,b) match {
       case (Some(val_a), Some(val_b)) => Some(f(val_a, val_b))
       case  _ => None
     }
+
+  // Exercise 4.4 - implement "sequence",
+  // turning a List[Option] into an Option[List]
+  def sequence[A](a: List[Option[A]]) : Option[List[A]] = {
+
+    /*
+    This works but has a worst-case runtime of 2n in the case where there are no Nones
+    (goes over the whole list to check for None, then over it again to extract the values from each Some).
+    There's an obvious way to do this O(n) iteratively (& must be an equivalent recursive/ declarative way as well).
+     */
+    // if (a.contains(None)) None else Some(a.map(elem=>elem.get))
+
+    /*
+    An implementation using foldRight and map2, again essentially cribbed from:
+    https://github.com/astorije/fpinscala-exercises/blob/master/src/main/scala/ch4errorhandling/Option.scala#L84
+     */
+    a.foldRight[Option[List[A]]](Some(List()))((oElem,oList)=>map2(oElem,oList)(_::_))
+    /*
+    map2 allows None values to be propagated through foldRight,
+    allowing screening for None and getting values to be done in a single pass
+    while also blocking further applications of the mapped function as soon as any None is found.
+     */
+
+  }
 
 }
--- src/test/scala/fp_in_scala/chapter_4/OptionFunctionsTests.scala
+++ src/test/scala/fp_in_scala/chapter_4/OptionFunctionsTests.scala
@@ -33,4 +33,32 @@ class OptionFunctionsTests extends Specification {
 
   }
 
+  "sequence function" should {
+
+    "return list of values where original list contains no Nones" in {
+      sequence(List(Some(1), Some(2), Some(3))) must beEqualTo(Some(List(1,2,3)))
+    }
+
+    "return empty list where original list is empty" in {
+      sequence(List()) must beEqualTo(Some(List()))
+    }
+
+    "return None where original list is all Nones" in {
+      sequence(List(None, None, None)) must beEqualTo(None)
+    }
+
+    "return None where original list starts with None" in {
+      sequence(List(None, Some(2), Some(3))) must beEqualTo(None)
+    }
+
+    "return None where original list ends with None" in {
+      sequence(List(Some(1), Some(2), None)) must beEqualTo(None)
+    }
+
+    "return None where original list has None in the middle" in {
+      sequence(List(Some(1), None, Some(3))) must beEqualTo(None)
+    }
+
+  }
+
 }
