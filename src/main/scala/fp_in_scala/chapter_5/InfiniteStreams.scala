e3d966783f18b246fbddf250c6035d9fca0a7247
Alex Garland <alex.d.garland@gmail.com>
2016-02-20 13:00:54 +0000

    Exercises 5.11, 5.12 - Implement unfold. Use it to reimplement constant, from and fibs

--- src/main/scala/fp_in_scala/chapter_5/InfiniteStreams.scala
+++ src/main/scala/fp_in_scala/chapter_5/InfiniteStreams.scala
@@ -6,18 +6,36 @@ import scala.collection.immutable.Stream.cons
 object InfiniteStreams {
 
 
-  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+  def constant[A](a: A): Stream[A] = {
+    // cons(a, constant(a))
+    unfold(a)(_ => Some(a, a))
+  }
 
 
-  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+  def from(n: Int): Stream[Int] = {
+    // cons(n, from(n + 1))
+    unfold(n)(i => Some(i, i+1))
+  }
 
 
   def fibs: Stream[Int] = {
 
-    def inner(a: Int, b: Int): Stream[Int] = cons(a, inner(b, a + b))
+//    def inner(a: Int, b: Int): Stream[Int] = cons(a, inner(b, a + b))
+//
+//    inner(0, 1)
 
-    inner(0, 1)
+    unfold((0, 1)) {
+      case(a, b) => Some((a, (b, a + b)))
+    }
 
+  }
+
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((a, s)) => cons(a, unfold(s)(f))
+      case None => Stream.empty
+    }
   }
 
 }
