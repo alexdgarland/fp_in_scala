f54f4d9f77552a447f355eeef0d122da4d42debf
Alex Garland <alex.d.garland@gmail.com>
2016-08-03 22:20:33 +0100

    Exercise 6.9 - reimplement Rand.map and Rand.map2 using Rand.flatMap

--- src/main/scala/fp_in_scala/chapter_6/RNG/Rand.scala
+++ src/main/scala/fp_in_scala/chapter_6/RNG/Rand.scala
@@ -13,18 +13,21 @@ object Rand {
 
 
   def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
+    flatMap(s)(
+      a =>
+        rng =>
+          (f(a), rng)
+    )
 
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-    rng => {
-      val (a, rng1) = ra(rng)
-      val (b, rng2) = rb(rng1)
-      (f(a, b), rng2)
-    }
+    flatMap(ra)(
+      a =>
+        rng1 => {
+          val (b, rng2) = rb(rng1)
+          (f(a, b), rng2)
+        }
+    )
   }
 
 
